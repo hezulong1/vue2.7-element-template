fbef1114ca5df0d4547f5c3540f3c80f25aabeff
hezulong1 <h2108b@163.com>
2026-08-13 17:29:56 +0800

    feat: 新增比较函数

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,5 +1,9 @@
 import type { Arrayable } from '@vueuse/core';
 
+export {
+  looseEqual,
+} from '@vue/shared';
+
 export function ensureArray<T>(arr?: Arrayable<T> | null): T[] {
   if (!arr && (arr as any) !== 0) return [];
   return Array.isArray(arr) ? arr : [arr as T];
