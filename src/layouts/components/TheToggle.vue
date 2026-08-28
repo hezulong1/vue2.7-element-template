e9fed7478a81b88e070a8edcc98f09ce7764c7b6
hezulong1 <h2108b@163.com>
2026-08-28 12:26:39 +0800

    perf: 重新调整样式，使用新的 vars 替代原先的 var

---
+++ src/layouts/components/TheToggle.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { Expand, Fold } from 'element-icons';
+import { useLayout } from '@/stores/layout';
+
+const { toggleSidebar, sidebarCollapsed } = useLayout();
+</script>
+
+<template>
+  <button :data-status="sidebarCollapsed.toString()" @click="() => toggleSidebar()">
+    <span v-show="sidebarCollapsed" class="el-icon"><Expand /></span>
+    <span v-show="!sidebarCollapsed" class="el-icon"><Fold /></span>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+@use 'sass:map';
+@use '@/styles/vars';
+
+button {
+  appearance: none;
+  background: none;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  width: 32px;
+  height: 32px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-self: center;
+  font-size: 16px;
+
+  &:hover {
+    background-color: rgba(0,0,0,.03);
+  }
+
+  &:active {
+    background-color: rgba(0,0,0,.15);
+  }
+
+  &:focus-visible {
+    outline: 2px solid map.get(vars.$el-colors, 'primary', 'light-5');
+    outline-offset: 1px;
+  }
+
+  svg {
+    font-size: 1em;
+  }
+}
+</style>
