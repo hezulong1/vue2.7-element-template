beaa02edbf46a04ddb1df978301b80b966c617e6
hezulong1 <h2108b@163.com>
2026-09-22 16:53:41 +0800

    feat(utils): [number] 添加 roundFloat 函数以支持浮点数四舍五入

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -2,3 +2,12 @@ export function looseToNumber(val: string) {
   const n = Number.parseFloat(val);
   return Number.isNaN(n) ? val : n;
 }
+
+/**
+ * @param n 值
+ * @param decimals 精度，默认为 0
+ */
+export function roundFloat(n: number, decimals = 0) {
+  n = Number(n.toString() + 'e' + decimals.toString());
+  return Number(Math.round(n) + 'e-' + decimals);
+}
